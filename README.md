ba2b296c9df627ad0c669c10d7a0d0e7c59752ee
Mwamodo Erick <erick@mwamodo.com>
2022-07-08 15:55:33 +0300

    WIP

--- README.md
+++ README.md
@@ -4,3 +4,9 @@ A mini personal finance system, made specifically for my needs. I doubt this wil
 starting point if you are looking to create something similar for yourself.
 
 - record income streams (track money in)
+- record expenses (track money out)
+- record spending plans & savings
+- record liabilities & assets
+- record and track goals (short-term 1-3 months, long-term 1-10 years)
+- record and track habits
+- record notes in md format
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,13 +12,11 @@ class DatabaseSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory()->create([
+            'name' => 'Mwamodo Erick',
+            'email' => 'erick@mwamodo.com',
+        ]);
     }
 }
